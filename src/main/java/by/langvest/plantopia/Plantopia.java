bd50d7cd377deb83582c82d6156394c3199fb41f
LanGvest <mister.langvest@gmail.com>
2023-11-24 18:55:28 +0300

    Setup

---
+++ src/main/java/by/langvest/plantopia/Plantopia.java
@@ -0,0 +1,10 @@
+package by.langvest.plantopia;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(Plantopia.MOD_ID)
+public class Plantopia {
+	public static final String MOD_ID = "plantopia";
+
+	public Plantopia() {}
+}
