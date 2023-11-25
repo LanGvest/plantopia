fb77b5d3e263b736a5dc1a8525738a5410726252
LanGvest <mister.langvest@gmail.com>
2023-11-25 13:31:26 +0300

    ✨ Added Plantopia Fluid Helper utility

---
+++ src/main/java/by/langvest/plantopia/util/PlantopiaFluidHelper.java
@@ -0,0 +1,19 @@
+package by.langvest.plantopia.util;
+
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.LevelReader;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.properties.BlockStateProperties;
+import org.jetbrains.annotations.NotNull;
+
+public class PlantopiaFluidHelper {
+	public static @NotNull BlockState getFluidBlockState(@NotNull Level level, BlockPos pos) {
+		return level.getFluidState(pos).createLegacyBlock().getBlock().defaultBlockState();
+	}
+
+	public static @NotNull BlockState copyWaterloggedFrom(LevelReader level, BlockPos pos, @NotNull BlockState state) {
+		if(!state.hasProperty(BlockStateProperties.WATERLOGGED)) return state;
+		return state.setValue(BlockStateProperties.WATERLOGGED, level.isWaterAt(pos));
+	}
+}
