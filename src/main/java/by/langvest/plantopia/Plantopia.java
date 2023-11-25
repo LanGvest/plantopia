5c8d38509e96e80144716506c22e0f21771efa1c
LanGvest <mister.langvest@gmail.com>
2023-11-25 11:44:40 +0300

    ✨ Added Log4j logger

--- src/main/java/by/langvest/plantopia/Plantopia.java
+++ src/main/java/by/langvest/plantopia/Plantopia.java
@@ -1,10 +1,15 @@
 package by.langvest.plantopia;
 
 import net.minecraftforge.fml.common.Mod;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 @Mod(Plantopia.MOD_ID)
 public class Plantopia {
 	public static final String MOD_ID = "plantopia";
+
+	@SuppressWarnings("unused")
+	public static final Logger LOGGER = LogManager.getLogger(MOD_ID);
 
 	public Plantopia() {}
 }
