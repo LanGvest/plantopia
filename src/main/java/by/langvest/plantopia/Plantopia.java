5527a02bf1b2b5e1123ba104fe15b58ca45be751
LanGvest <mister.langvest@gmail.com>
2023-11-25 11:59:46 +0300

    ✨ Added mod event bus

--- src/main/java/by/langvest/plantopia/Plantopia.java
+++ src/main/java/by/langvest/plantopia/Plantopia.java
@@ -1,6 +1,8 @@
 package by.langvest.plantopia;
 
+import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -11,5 +13,7 @@ public class Plantopia {
 	@SuppressWarnings("unused")
 	public static final Logger LOGGER = LogManager.getLogger(MOD_ID);
 
-	public Plantopia() {}
+	public Plantopia() {
+		IEventBus bus = FMLJavaModLoadingContext.get().getModEventBus();
+	}
 }
