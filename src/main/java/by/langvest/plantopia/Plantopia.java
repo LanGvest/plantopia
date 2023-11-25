2ea92622fc45c341186871a9e72d59977b4efa10
LanGvest <mister.langvest@gmail.com>
2023-11-25 12:02:03 +0300

    ✨ Added common setup mod event

--- src/main/java/by/langvest/plantopia/Plantopia.java
+++ src/main/java/by/langvest/plantopia/Plantopia.java
@@ -2,9 +2,11 @@ package by.langvest.plantopia;
 
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.jetbrains.annotations.NotNull;
 
 @Mod(Plantopia.MOD_ID)
 public class Plantopia {
@@ -15,5 +17,8 @@ public class Plantopia {
 
 	public Plantopia() {
 		IEventBus bus = FMLJavaModLoadingContext.get().getModEventBus();
+		bus.addListener(this::commonSetup);
 	}
+
+	private void commonSetup(final @NotNull FMLCommonSetupEvent event) {}
 }
