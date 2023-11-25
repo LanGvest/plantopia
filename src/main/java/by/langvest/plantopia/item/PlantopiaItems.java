2ac4fae4722135cfcebb267cc55cfff4b93b154c
LanGvest <mister.langvest@gmail.com>
2023-11-25 12:30:24 +0300

    🩹 Fixed registerItem() method return type

--- src/main/java/by/langvest/plantopia/item/PlantopiaItems.java
+++ src/main/java/by/langvest/plantopia/item/PlantopiaItems.java
@@ -5,14 +5,15 @@ import net.minecraft.world.item.Item;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
 
 import java.util.function.Supplier;
 
 public class PlantopiaItems {
 	private static final DeferredRegister<Item> ITEM_REGISTER = DeferredRegister.create(ForgeRegistries.ITEMS, Plantopia.MOD_ID);
 
-	public static <T extends Item> void registerItem(String name, Supplier<T> supplier) {
-		ITEM_REGISTER.register(name, supplier);
+	public static <T extends Item> RegistryObject<T> registerItem(String name, Supplier<T> supplier) {
+		return ITEM_REGISTER.register(name, supplier);
 	}
 
 	public static void setup(IEventBus bus) {
