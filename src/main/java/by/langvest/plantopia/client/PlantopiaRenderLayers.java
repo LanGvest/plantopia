bf9830f961a1a65905fe4a357f0cfbbd8055aa2d
LanGvest <mister.langvest@gmail.com>
2023-11-29 15:18:31 +0300

    ✨ Added block render layers processing

--- src/main/java/by/langvest/plantopia/client/PlantopiaRenderLayers.java
+++ src/main/java/by/langvest/plantopia/client/PlantopiaRenderLayers.java
@@ -1,9 +1,24 @@
 package by.langvest.plantopia.client;
 
+import by.langvest.plantopia.util.semantics.PlantopiaSemanticRegistry;
+import net.minecraft.client.renderer.ItemBlockRenderTypes;
+import net.minecraft.client.renderer.RenderType;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
+import org.jetbrains.annotations.Nullable;
 
 @OnlyIn(Dist.CLIENT)
 public class PlantopiaRenderLayers {
-	public static void setup() {}
+	public static void setup() {
+		PlantopiaSemanticRegistry.getBlocks().forEach(blockSemantics -> {
+			@Nullable RenderType renderType = switch(blockSemantics.getRenderType()) {
+				case CUTOUT -> RenderType.cutout();
+				case TRANSLUCENT -> RenderType.translucent();
+				default -> null;
+			};
+
+			if(renderType == null) return;
+			ItemBlockRenderTypes.setRenderLayer(blockSemantics.getBlock(), renderType);
+		});
+	}
 }
