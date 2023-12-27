3755654b772db7933b0fb751642f9f14d9b7a229
LanGvest <mister.langvest@gmail.com>
2023-12-27 13:17:56 +0300

    ✏️ Changed Plantopia creative mode tab icon from dirt to fireweed

--- src/main/java/by/langvest/plantopia/tab/PlantopiaCreativeModeTabs.java
+++ src/main/java/by/langvest/plantopia/tab/PlantopiaCreativeModeTabs.java
@@ -1,9 +1,9 @@
 package by.langvest.plantopia.tab;
 
 import by.langvest.plantopia.Plantopia;
+import by.langvest.plantopia.block.PlantopiaBlocks;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.level.block.Blocks;
 import org.jetbrains.annotations.Contract;
 import org.jetbrains.annotations.NotNull;
 
@@ -12,7 +12,7 @@ public class PlantopiaCreativeModeTabs {
 		@Override
 		@Contract(" -> new")
 		public @NotNull ItemStack makeIcon() {
-			return new ItemStack(Blocks.DIRT);
+			return new ItemStack(PlantopiaBlocks.FIREWEED.get());
 		}
 	};
 }
