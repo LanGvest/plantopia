1fafba278296951c5737bdd004fcda569d7fcf13
LanGvest <mister.langvest@gmail.com>
2023-12-28 17:43:49 +0300

    ✨ Added flammability to certain vanilla organic blocks

--- src/main/java/by/langvest/plantopia/block/PlantopiaCompats.java
+++ src/main/java/by/langvest/plantopia/block/PlantopiaCompats.java
@@ -6,12 +6,29 @@ import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.ComposterBlock;
 import net.minecraft.world.level.block.FireBlock;
+import org.jetbrains.annotations.NotNull;
 
 public class PlantopiaCompats {
 	public static void setup() {
+		registerAll();
+
+		registerFlammable(Blocks.OAK_SAPLING, 60, 100);
+		registerFlammable(Blocks.SPRUCE_SAPLING, 60, 100);
+		registerFlammable(Blocks.BIRCH_SAPLING, 60, 100);
+		registerFlammable(Blocks.JUNGLE_SAPLING, 60, 100);
+		registerFlammable(Blocks.ACACIA_SAPLING, 60, 100);
+		registerFlammable(Blocks.DARK_OAK_SAPLING, 60, 100);
+		registerFlammable(Blocks.BAMBOO_SAPLING, 60, 100);
+		registerFlammable(Blocks.SUGAR_CANE, 60, 100);
+		registerFlammable(Blocks.SEA_PICKLE, 60, 100);
+		registerFlammable(Blocks.MOSS_BLOCK, 60, 100);
+		registerFlammable(Blocks.MOSS_CARPET, 60, 100);
+	}
+
+	private static void registerAll() {
 		PlantopiaMetaStore.getBlocks().forEach(blockMeta -> {
 			if(blockMeta.isFlammable()) registerFlammable(blockMeta.getBlock(), blockMeta.getEncouragement(), blockMeta.getFlammability());
-			if(blockMeta.isCompostable()) registerCompostable(blockMeta.getBlock().asItem(), blockMeta.getCompostability());
+			if(blockMeta.isCompostable()) registerCompostable(blockMeta.getBlock(), blockMeta.getCompostability());
 		});
 	}
 
@@ -20,7 +37,7 @@ public class PlantopiaCompats {
 		fireBlock.setFlammable(block, encouragement, flammability);
 	}
 
-	public static void registerCompostable(ItemLike item, float compostability) {
-		ComposterBlock.COMPOSTABLES.put(item, compostability);
+	public static void registerCompostable(@NotNull ItemLike item, float compostability) {
+		ComposterBlock.COMPOSTABLES.put(item.asItem(), compostability);
 	}
 }
