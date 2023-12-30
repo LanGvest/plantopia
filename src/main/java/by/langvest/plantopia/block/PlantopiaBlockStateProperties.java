16e2316675411e4ba7a144595c38aeb49f03fd77
LanGvest <mister.langvest@gmail.com>
2023-12-30 13:22:56 +0300

    ✨ Added block state properties

---
+++ src/main/java/by/langvest/plantopia/block/PlantopiaBlockStateProperties.java
@@ -0,0 +1,11 @@
+package by.langvest.plantopia.block;
+
+import net.minecraft.world.level.block.state.properties.EnumProperty;
+import net.minecraft.world.level.block.state.properties.IntegerProperty;
+
+public class PlantopiaBlockStateProperties {
+	public static final IntegerProperty FLOWER_AMOUNT = IntegerProperty.create("flower_amount", 1, 4);
+	public static final IntegerProperty LEAF_AMOUNT = IntegerProperty.create("leaf_amount", 1, 4);
+	public static final IntegerProperty POLLINATION_COUNT = IntegerProperty.create("pollination_count", 1, 4);
+	public static final EnumProperty<PlantopiaTripleBlockHalf> TRIPLE_BLOCK_HALF = EnumProperty.create("half", PlantopiaTripleBlockHalf.class);
+}
