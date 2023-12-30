77ae24b79bf940c5f68379e47f24e97b93c71fdd
LanGvest <mister.langvest@gmail.com>
2023-12-30 14:48:56 +0300

    ✏️ Refactored render layers class

--- src/main/java/by/langvest/plantopia/client/PlantopiaRenderLayers.java
+++ src/main/java/by/langvest/plantopia/client/PlantopiaRenderLayers.java
@@ -1,35 +1,63 @@
 package by.langvest.plantopia.client;
 
-import by.langvest.plantopia.meta.PlantopiaBlockMeta;
 import by.langvest.plantopia.meta.PlantopiaMetaStore;
+import by.langvest.plantopia.meta.property.PlantopiaRenderType;
+import com.google.common.collect.Sets;
 import net.minecraft.client.renderer.ItemBlockRenderTypes;
 import net.minecraft.client.renderer.RenderType;
+import net.minecraft.world.level.block.Block;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
 import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
+
+import java.util.List;
+import java.util.Set;
 
 @OnlyIn(Dist.CLIENT)
 public class PlantopiaRenderLayers {
+	private static final Set<Block> CUTOUT = Sets.newHashSet();
+	private static final Set<Block> CUTOUT_MIPPED = Sets.newHashSet();
+	private static final Set<Block> TRANSLUCENT = Sets.newHashSet();
+
 	public static void setup() {
+		registerAll();
 		setAll();
 	}
 
-	private static void setAll() {
+	private static void add(@NotNull Set<Block> blockSet, Block... blocks) {
+		blockSet.addAll(List.of(blocks));
+	}
+
+	private static void registerAll() {
 		PlantopiaMetaStore.getBlocks().forEach(blockMeta -> {
-			RenderType renderType = getRenderType(blockMeta);
-			if(renderType == null) return;
-			ItemBlockRenderTypes.setRenderLayer(blockMeta.getBlock(), renderType);
+			if(!blockMeta.shouldApplyRenderLayer()) return;
+
+			Block block = blockMeta.getBlock();
+			PlantopiaRenderType renderType = blockMeta.getRenderType();
+
+			if(renderType == PlantopiaRenderType.CUTOUT) {
+				CUTOUT.add(block);
+				return;
+			}
+
+			if(renderType == PlantopiaRenderType.CUTOUT_MIPPED) {
+				CUTOUT_MIPPED.add(block);
+				return;
+			}
+
+			if(renderType == PlantopiaRenderType.TRANSLUCENT) {
+				TRANSLUCENT.add(block);
+			}
 		});
 	}
 
-	@Nullable
-	private static RenderType getRenderType(@NotNull PlantopiaBlockMeta blockMeta) {
-		return switch(blockMeta.getRenderType()) {
-			case CUTOUT -> RenderType.cutout();
-			case CUTOUT_MIPPED -> RenderType.cutoutMipped();
-			case TRANSLUCENT -> RenderType.translucent();
-			default -> null;
-		};
+	private static void setAll() {
+		RenderType cutout = RenderType.cutout();
+		RenderType cutoutMipped = RenderType.cutoutMipped();
+		RenderType translucent = RenderType.translucent();
+
+		for(Block block : CUTOUT) ItemBlockRenderTypes.setRenderLayer(block, cutout);
+		for(Block block : CUTOUT_MIPPED) ItemBlockRenderTypes.setRenderLayer(block, cutoutMipped);
+		for(Block block : TRANSLUCENT) ItemBlockRenderTypes.setRenderLayer(block, translucent);
 	}
 }
