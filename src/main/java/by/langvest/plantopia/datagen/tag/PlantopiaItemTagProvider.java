63047ff462d1153e57f784b5ddbae766f9a5d89f
LanGvest <mister.langvest@gmail.com>
2024-01-02 19:22:56 +0300

    🩹 Fixed missing item tag data generation

--- src/main/java/by/langvest/plantopia/datagen/tag/PlantopiaItemTagProvider.java
+++ src/main/java/by/langvest/plantopia/datagen/tag/PlantopiaItemTagProvider.java
@@ -1,6 +1,7 @@
 package by.langvest.plantopia.datagen.tag;
 
 import by.langvest.plantopia.Plantopia;
+import by.langvest.plantopia.meta.PlantopiaBlockMeta;
 import by.langvest.plantopia.meta.PlantopiaMetaStore;
 import by.langvest.plantopia.tag.PlantopiaItemTags;
 import by.langvest.plantopia.util.PlantopiaTagSet;
@@ -8,6 +9,7 @@ import net.minecraft.data.DataGenerator;
 import net.minecraft.data.tags.BlockTagsProvider;
 import net.minecraft.data.tags.ItemTagsProvider;
 import net.minecraft.data.tags.TagsProvider;
+import net.minecraft.tags.ItemTags;
 import net.minecraft.tags.TagKey;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.level.ItemLike;
@@ -21,6 +23,10 @@ import java.util.Arrays;
 import java.util.Objects;
 
 public class PlantopiaItemTagProvider extends ItemTagsProvider {
+	private final PlantopiaTagSet<Item> TALL_FLOWERS = PlantopiaTagSet.newTagSet();
+	private final PlantopiaTagSet<Item> SMALL_FLOWERS = PlantopiaTagSet.newTagSet();
+	private final PlantopiaTagSet<Item> LEAVES = PlantopiaTagSet.newTagSet();
+	private final PlantopiaTagSet<Item> SAPLINGS = PlantopiaTagSet.newTagSet();
 	private final PlantopiaTagSet<Item> IGNORED_BY_BEES = PlantopiaTagSet.newTagSet();
 	private final PlantopiaTagSet<Item> PREFERRED_BY_BEES = PlantopiaTagSet.newTagSet();
 
@@ -51,13 +57,28 @@ public class PlantopiaItemTagProvider extends ItemTagsProvider {
 			if(!blockMeta.hasItem()) return;
 
 			Block block = blockMeta.getBlock();
+			Item item = block.asItem();
+			PlantopiaBlockMeta.MetaType type = blockMeta.getType();
+			int baseHigh = blockMeta.getBlockHighType().getBaseHigh();
 
-			if(blockMeta.isIgnoredByBees()) IGNORED_BY_BEES.add(block.asItem());
-			if(blockMeta.isPreferredByBees()) PREFERRED_BY_BEES.add(block.asItem());
+			if(blockMeta.isIgnoredByBees()) IGNORED_BY_BEES.add(item);
+			if(blockMeta.isPreferredByBees()) PREFERRED_BY_BEES.add(item);
+
+			if(type.isFlowerLike()) {
+				if(baseHigh > 1) TALL_FLOWERS.add(item);
+				else SMALL_FLOWERS.add(item);
+			}
+
+			if(type.isLeavesLike()) LEAVES.add(item);
+			if(type.isSaplingLike()) SAPLINGS.add(item);
 		});
 	}
 
 	private void saveAll() {
+		save(ItemTags.TALL_FLOWERS, TALL_FLOWERS);
+		save(ItemTags.SMALL_FLOWERS, SMALL_FLOWERS);
+		save(ItemTags.LEAVES, LEAVES);
+		save(ItemTags.SAPLINGS, SAPLINGS);
 		save(PlantopiaItemTags.IGNORED_BY_BEES, IGNORED_BY_BEES);
 		save(PlantopiaItemTags.PREFERRED_BY_BEES, PREFERRED_BY_BEES);
 	}
